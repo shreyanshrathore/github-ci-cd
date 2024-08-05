53b19db6ee1fa5d428a56c711d23e6ceb7a7be50
shreyansh-error-89 <shreyanshrathore75649@gmail.com>
2024-08-05 14:58:21 +0530

    Fix: ⬆ package versions

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
+import express from "express";
+
+const app = express();
+
+app.listen(3000, () => console.log("Hello from 3000"));
+
+app.get("/health", (req, res) => res.send("Hello server is up"));
+
 console.log("Hello World");
-export const sayHello = (name: string) => `Hello, ${name}`
+export const sayHello = (name: string) => `Hello, ${name}`;
