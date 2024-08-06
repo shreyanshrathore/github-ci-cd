c9ab89abb67504cb9312baec2a1ccb8996e7e51a
shreyansh-error-89 <shreyanshrathore75649@gmail.com>
2024-08-06 11:15:12 +0530

    rm: 🔥 removed server code

--- __tests__/sayhello.test.ts
+++ __tests__/sayhello.test.ts
@@ -1,11 +1,10 @@
-import { sayHello } from '../src/index'; // Adjust the import path
+import { sayHello } from "../src/index"; // Adjust the import path
 
-describe('sayHello', () => {
-  it('should return a greeting message with the given name', () => {
-    const name = 'John';
+describe("sayHello", () => {
+  it("should return a greeting message with the given name", () => {
+    const name = "John";
     const expected = `Hello, ${name}`;
-    
+
     expect(sayHello(name)).toBe(expected);
   });
 });
-
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import express from "express";
+// import express from "express";
 
-const app = express();
+// const app = express();
 
-app.listen(3000, () => console.log("Hello from 3000"));
+// app.listen(3000, () => console.log("Hello from 3000"));
 
-app.get("/health", (req, res) => res.send("Hello server is up"));
+// app.get("/health", (req, res) => res.send("Hello server is up"));
 
 console.log("Hello World");
 export const sayHello = (name: string) => `Hello, ${name}`;
